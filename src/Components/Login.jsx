1b63b251ec1c7e41963daca68a11ef48733919cd
mrudulpatel <mrudulpatel04@gmail.com>
2022-02-19 20:35:41 +0530

    second-commit

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { Timestamp, addDoc, collection } from "firebase/firestore";
 import "../Styling/Login.css";
+import { db } from "../firebase/firebase";
 
 const Login = () => {
+  const [data, setData] = useState({
+    name: "",
+    mobile_no: "",
+    error: null,
+  });
+  const { name, mobile_no } = data;
+  const handleChangeName = (e) => {
+    e.preventDefault();
+    setData({
+      ...data,
+      name: e.target.value,
+    });
+    console.log(data);
+  };
+  const handleChangeMobile = (e) => {
+    e.preventDefault();
+    setData({
+      ...data,
+      mobile_no: e.target.value,
+    });
+    console.log(data);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!name || !mobile_no) {
+      alert("Please enter name and mobile number!");
+    } else {
+      try {
+        await addDoc(collection(db, "users"), {
+          name,
+          mobile_no,
+          createdAt: Timestamp.fromDate(new Date()),
+        });
+        setData({
+          name: "",
+          mobile_no: "",
+          error: null,
+        });
+      } catch (error) {
+        setData({ ...data, error: error.message });
+      }
+    }
+  };
+
   return (
     <div className="main_login">
       <h3>UpMenu</h3>
@@ -13,9 +60,17 @@ const Login = () => {
       <div className="input_fields">
         <p>Lets get you to our canteen menu</p>
         <div className="creds">
-          <input type="text" placeholder="Enter your name" />
-          <input type="tel" placeholder="Enter Your mobile number" />
-          <button>Lesgo </button>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            onChange={handleChangeName}
+          />
+          <input
+            type="tel"
+            placeholder="Enter Your mobile number"
+            onChange={handleChangeMobile}
+          />
+          <button onClick={handleSubmit}>Lesgo</button>
         </div>
       </div>
     </div>
