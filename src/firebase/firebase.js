b6b615481b6ec762ecca0807d45ba6ae970c6f93
mrudulpatel <mrudulpatel04@gmail.com>
2022-02-19 20:32:14 +0530

    Create firebase.js

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBABSvy6tKA2QK4q6H7HxbuLIGHGFJC2iM",
+  authDomain: "canteen-token-system.firebaseapp.com",
+  projectId: "canteen-token-system",
+  storageBucket: "canteen-token-system.appspot.com",
+  messagingSenderId: "1076575577836",
+  appId: "1:1076575577836:web:2158754d2f3b270a45e7c9",
+  measurementId: "G-VG32JGJ0EN",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
