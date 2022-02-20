1864005d5beee1db288e8e8c34ca8bd015e7d2f3
mrudulpatel <mrudulpatel04@gmail.com>
2022-02-20 15:27:04 +0530

    fifth-commit

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,4 +1,3 @@
-// import { collection, query, where, onSnapshot, doc } from "firebase/firestore";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "../firebase/firebase";
